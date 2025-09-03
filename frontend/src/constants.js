63995b5e2ffa131d4c9f97b3c20530ac6905145c
Viktor Kherkunov <ViktorWinn@mail.ru>
2025-09-03 12:02:58 +0300

    predeploy3

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
-export const frontendBaseURL = "http://localhost:5173";
-export const backendBaseURL = "http://127.0.0.1:8000";
+export const frontendBaseURL = import.meta.env.VITE_FRONTEND_BASE_URL;
+export const backendBaseURL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
